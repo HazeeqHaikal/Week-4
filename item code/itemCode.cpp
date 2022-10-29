a9425b004fe473262a0437cb4cef2420c735e001
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-10-29 20:02:16 +0800

    final push

--- item code/itemCode.cpp
+++ item code/itemCode.cpp
@@ -13,8 +13,29 @@ int main() {
                        0.07,
                        0.1,
                        0.1};
-    
 
-    
+    char choice;
+    float chargeOuput;
+    float priceOutput;
+    cout << "Enter the item code: ";
+    cin >> choice;
+
+    choice = toupper(choice);
+
+    bool found = false;
+    for (int i = 0; i < sizeof(itemCode); i++) {
+        if (choice == itemCode[i]) {
+            chargeOuput = price[i] * charge[i];
+            priceOutput = price[i] + chargeOuput;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "Error, this is not in the list";
+    }
+
+    cout << "Final price: RM" << priceOutput;
+
     return 0;
 }
