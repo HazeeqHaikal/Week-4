f3741a192c784c5e5d674ecef3b6c6f2edef4b20
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-11-01 11:33:00 +0800

    fixed investment and dividend

--- investment and dividend/investmentDividend.cpp
+++ investment and dividend/investmentDividend.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -10,17 +11,19 @@ int main() {
     cin >> investment;
 
     if (investment > 75000) {
-        dividend = 0.013 * investment;
+        dividend = 1.3 / 100 * investment;
     } else if (investment > 50000) {
-        dividend = 0.011 * investment;
+        dividend = 1.1 / 100 * investment;
     } else if (investment > 30000) {
-        dividend = 0.01 * investment;
+        dividend = 1.0 / 100 * investment;
     } else if (investment > 10000) {
-        dividend = 0.003 * investment;
+        dividend = 0.8 / 100 * investment;
+    } else if (investment > 5000) {
+        dividend = 0.3 / 100 * investment;
     } else {
-        dividend = 0.002 * investment;
+        dividend = 0.2 / 100 * investment;
     }
 
-    cout << "\nThe dividend is: RM" << dividend << endl;
+    cout << fixed << setprecision(2) << "\nThe dividend is: RM" << dividend << endl;
     return 0;
 }
