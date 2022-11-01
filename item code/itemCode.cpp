af2cf313f38288b05e346cc2e5c18209ce70d820
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-11-01 11:42:24 +0800

    minor update item code v2

--- item code/itemCode.cpp
+++ item code/itemCode.cpp
@@ -2,17 +2,18 @@
 #include <string>
 
 using namespace std;
+#define count 7
 
 int main() {
-    char itemCode[7] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-    float price[7] = {54.00, 65.00, 82.00, 103.00, 150.00, 245.00, 250};
-    float charge[7] = {0.05,
-                       0.05,
-                       0.05,
-                       0.07,
-                       0.07,
-                       0.1,
-                       0.1};
+    char itemCode[count] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+    float price[count] = {54.00, 65.00, 82.00, 103.00, 150.00, 245.00, 250};
+    float charge[count] = {0.05,
+                           0.05,
+                           0.05,
+                           0.07,
+                           0.07,
+                           0.1,
+                           0.1};
 
     char choice;
     float chargeOuput;
@@ -23,7 +24,7 @@ int main() {
     choice = toupper(choice);
 
     bool found = false;
-    for (int i = 0; i < sizeof(itemCode); i++) {
+    for (int i = 0; i < sizeof(itemCode) / sizeof(itemCode[0]); i++) {
         if (choice == itemCode[i]) {
             chargeOuput = price[i] * charge[i];
             priceOutput = price[i] + chargeOuput;
