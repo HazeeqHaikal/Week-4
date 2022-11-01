fcfe45d23d94f9cc983bb673ea5a20e24e431a6b
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-11-01 10:22:23 +0800

    update

--- bonus/bonus.cpp
+++ bonus/bonus.cpp
@@ -1,21 +1,36 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    int performanceMark, bonus;
+    int mark;
+    float bonus;
+    string name;
 
-    cout << "Enter performance mark: ";
-    cin >> performanceMark;
+    cout << "Enter your name: ";
+    getline(cin, name);
 
-    if (performanceMark >= 6 && performanceMark <= 10) {
+    cout << "Enter your performance mark: ";
+    cin >> mark;
+
+    if (mark >= 6 && mark <= 10) {
         bonus = 2500;
-    } else if (performanceMark < 6) {
+    } else if (mark < 6) {
         bonus = 1200;
     }
 
-    cout << "Bonus: " << bonus;
+    // make name variable capitalize on every first word
+    for (int i = 0; i < name.length(); i++) {
+        if (i == 0) {
+            name[i] = toupper(name[i]);
+        } else if (name[i] == ' ') {
+            name[i + 1] = toupper(name[i + 1]);
+        }
+    }
+
+    cout << endl << fixed << setprecision(2) << name << " bonus is: RM" << bonus;
 
     return 0;
 }
