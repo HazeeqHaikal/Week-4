23c3b3a0d84f33bcfa6e96fbfc0604e2267519b2
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-11-01 11:20:24 +0800

    easier math for percentage

--- discount/discount.cpp
+++ discount/discount.cpp
@@ -4,22 +4,24 @@
 using namespace std;
 
 int main() {
-
     float price, discount, priceAfterDiscount;
+
+    discount = 0;
 
     cout << "Enter the price of the item: ";
     cin >> price;
 
-    if(price >= 300 && price <= 1000){
-        discount = price * 0.055;
-    }else if(price > 1000){
-        discount = price * 0.065;
+    if (price >= 300 && price <= 1000) {
+        discount = price * 5.5 / 100;
+    }
+
+    if (price > 1000) {
+        discount = price * 6.5 / 100;
     }
 
     priceAfterDiscount = price - discount;
 
-    cout << "Price of the item after discount: RM" << priceAfterDiscount << endl;
+    cout << endl << "Price of the item after discount: RM" << priceAfterDiscount << endl;
 
     return 0;
-
 }
