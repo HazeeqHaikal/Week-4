ba32b9a885bf2e976624551c0c4754a85384c9bf
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-11-08 10:15:30 +0800

    fixed

--- scholarship/scholarship.cpp
+++ scholarship/scholarship.cpp
@@ -4,14 +4,16 @@
 using namespace std;
 
 int main() {
-    char program;
+    string program;
     float cgpa, amount;
 
-    cout << "\nWhat is your program?\nA. Diploma\nB. Bachelor\n\nYour program: ";
+    cout << "\nWhat is your program? (Diploma, Bachelor)\n\nYour program: ";
     cin >> program;
 
     // uppercase char for program variable
-    program = toupper(program);
+    for (auto& c : program) {
+        c = toupper(c);
+    }
 
     cout << "What is your CGPA: ";
     cin >> cgpa;
@@ -20,13 +22,13 @@ int main() {
         cout << "Invalid CGPA" << endl;
         main();
         return 1;
-    } else if (program == 'A' && cgpa >= 3.0) {
+    } else if (program == "DIPLOMA" && cgpa >= 3.0) {
         amount = 7000;
-    } else if (program == 'A' && cgpa < 3.0) {
+    } else if (program == "DIPLOMA" && cgpa < 3.0) {
         amount = 5000;
-    } else if (program == 'B' && cgpa >= 3.0) {
+    } else if (program == "BACHELOR" && cgpa >= 3.0) {
         amount = 10000;
-    } else if (program == 'B' && cgpa < 3.0) {
+    } else if (program == "BACHELOR" && cgpa < 3.0) {
         amount = 6000;
     } else {
         cout << endl
